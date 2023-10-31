06d6afdde6dc8bcbc93a6ea753d850b1e81e9077
soyakkumarkp <soyalkumarkp@gmail.com>
2023-10-31 17:06:13 +0530

    created component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import PageNotFound from "./components/PageNotFound";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import Username from "./components/Username";
 
 const App = () => {
-  return <div>React login app</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Username />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/password" element={<Password />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/recovery" element={<Recovery />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
